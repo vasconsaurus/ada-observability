8c823cbf126b49d80bc1876c35c4360dca5dd7ce
manu vasconcelos <manu.vasconcelos@gmail.com>
2023-02-10 20:09:23 -0300

    add port

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask, render_Template
+from flask import Flask, render_template
 from prometheus_flask_exporter import PrometheusMetrics
 
 app = Flask(__name__)
 metrics = PrometheusMetrics(app)
 
 
-@app.route('/renda-fixa')
+@app.route("/renda-fixa")
 def renda_fixa():
-    return render_template('lista.hmtl', title='Renda Fixa')
+    return render_template("lista.html", title="Renda Fixa")
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5001")
