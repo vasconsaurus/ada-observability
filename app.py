b04d6282d86b948a1c464c844876e434034744d4
manu vasconcelos <manu.vasconcelos@gmail.com>
2023-02-17 20:12:33 -0300

    simula usuarios

--- app.py
+++ app.py
@@ -2,10 +2,14 @@ import time
 import random
 
 from flask import Flask, render_template
+import prometheus_client as prom
 from prometheus_flask_exporter import PrometheusMetrics
 
 app = Flask(__name__)
 metrics = PrometheusMetrics(app)
+
+quantidade_usuarios_online = prom.Gauge(
+    "quantidade_usuarios_online", "Números de usuários online no momento")
 
 
 @app.route("/renda-fixa")
@@ -14,6 +18,7 @@ metrics = PrometheusMetrics(app)
                  labels={"tipo": "ACOES"})
 def renda_fixa():
     time.sleep(random.randint(1, 10))
+    quantidade_usuarios_online.set(random.randint(1, 100))
     return render_template("lista.html", title="Renda Fixa")
 
 
